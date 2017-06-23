7b18e965af85642f53b6eabbf9d5d11847068356
jjq <cz_jjq@163.com>
2017-06-23 11:29:38 +0800

    upgrade

--- src/json-schema-js/JsonSchema.js
+++ src/json-schema-js/JsonSchema.js
@@ -413,8 +413,8 @@ JsonSchema.prototype.buildObjectStackItem = function(object,name){
 };
 
 JsonSchema.prototype.getDefaultInstance = function(schema){
-    var object_stack= [this.buildObjectStackItem(null,ROOT_KEY_NAME)];
-    var schema_stack;
+    let object_stack = [this.buildObjectStackItem(null,ROOT_KEY_NAME)];
+    let schema_stack;
     if (typeof schema === "string" ){
         schema_stack = [this.schema[schema]];
     }else{
@@ -427,13 +427,13 @@ JsonSchema.prototype.getDefaultInstance = function(schema){
 };
 
 JsonSchema.prototype.generateDefaultValue = function(schema_stack, object_stack){
-    var sl=schema_stack.length-1,ol=object_stack.length-1;
-    var schema=schema_stack[sl],object=object_stack[ol].object;
-    var name=object_stack[ol].key,prop=null;//prop指向当前处理的对象，其实返回时才需要使用
-    var uicontrol=schema["uicontrol"];//获取uicontrol信息,dont care any type
-    var parentSchema=null;
-    if(sl>0){//如果不是堆栈底部，则获取父级schema
-        parentSchema= schema_stack[sl-1];
+    let sl = schema_stack.length - 1,ol = object_stack.length - 1;
+    let schema = schema_stack[sl],object = object_stack[ol].object;
+    let name = object_stack[ol].key,prop = null;//prop指向当前处理的对象，其实返回时才需要使用
+    let uicontrol = schema["uicontrol"];//获取uicontrol信息,dont care any type
+    let parentSchema = null;
+    if(sl > 0){//如果不是堆栈底部，则获取父级schema
+        parentSchema = schema_stack[sl - 1];
     }
 
     if(object){//如果对象存在（第一次迭代时一定为null）
@@ -442,42 +442,42 @@ JsonSchema.prototype.generateDefaultValue = function(schema_stack, object_stack)
 
     if(schema.hasOwnProperty("type")){
 
-        if(typeof schema.type==="string"){
+        if(typeof schema.type === "string"){
 
             if(this.fieldDefaultValue.hasOwnProperty(schema.type)){
-                prop=this.fieldDefaultValue[schema.type](schema);
+                prop = this.fieldDefaultValue[schema.type](schema);
                 object_stack.push(this.buildObjectStackItem(prop,name));
             }else{
-                throw new Error("fieldDefaultValue中 Type="+schema.type+"没有设定DefaultValue处理");
+                throw new Error("fieldDefaultValue中 Type=" + schema.type + "没有设定DefaultValue处理");
             }
             //observer.next({"parentSchema":parentSchema,"currentSchema":{"name":name,"schema":schema}});
 
-            if (schema.type==="object" && schema.properties && typeof schema.properties==="object"){
-                for(var v in schema.properties){
+            if (schema.type === "object" && schema.properties && typeof schema.properties === "object"){
+                for(let v in schema.properties){
 
-                    var new_schema_stack=clone(schema_stack);
+                    let new_schema_stack = clone(schema_stack);
                     //如果出现$ref，则生成新的schema_stack，并重新处理
                     if(schema.properties[v].hasOwnProperty("$ref")){
-                        new_schema_stack=this.resolveURI(new_schema_stack,schema.properties[v].$ref);
+                        new_schema_stack = this.resolveURI(new_schema_stack,schema.properties[v].$ref);
                     }else{
                         new_schema_stack.push(schema.properties[v]);
                     }
                     /*start 开始处理parent与current的对应关系，初始化TEMP_REF_PROP_NAME*/
-                    var new_schema=new_schema_stack[new_schema_stack.length-1];
+                    let new_schema = new_schema_stack[new_schema_stack.length - 1];
                     if(schema.properties[v].hasOwnProperty("$ref")){
-                        new_schema[TEMP_REF_PROP_NAME]=v;
+                        new_schema[TEMP_REF_PROP_NAME] = v;
                     }
 
-                    var parentName=null;
+                    let parentName = null;
                     if(schema[TEMP_REF_PROP_NAME]){
-                        parentName=schema[TEMP_REF_PROP_NAME];
+                        parentName = schema[TEMP_REF_PROP_NAME];
                     }
                     //observer.next({"parentSchema":{"name":parentName,"uicontrol":parentSchema?parentSchema["uicontrol"]:null,"schema":parentSchema},"currentSchema":{"name":v,"uicontrol":new_schema["uicontrol"],"schema":new_schema}});
 
                     //生成默认值只需要建schema_stack，并不需要用object_stack
                     //let new_object_stack:ObjectStackItem[]=clone(object_stack);
                     //new_object_stack.push(this.buildObjectStackItem());
-                    prop[v]=this.generateDefaultValue(new_schema_stack,object_stack);
+                    prop[v] = this.generateDefaultValue(new_schema_stack,object_stack);
 
                     /*end 完成TEMP_REF_PROP_NAME 的处理，进行清理工作*/
                     if(new_schema[TEMP_REF_PROP_NAME]){
@@ -486,14 +486,14 @@ JsonSchema.prototype.generateDefaultValue = function(schema_stack, object_stack)
                 }
             }
         }else if(Array.isArray(schema.type)){//如果type是数组，则只能使用default字段的值，否则报错
-            prop=this.fieldDefaultValue["array"](schema);
+            prop = this.fieldDefaultValue["array"](schema);
             object_stack.push(prop);
         }
         else{
-            throw new Error("fieldDefaultValue中 Type="+schema.type+"没有合适的方法进行处理，仅支持string和array");
+            throw new Error("fieldDefaultValue中 Type=" + schema.type + "没有合适的方法进行处理，仅支持string和array");
         }
     }else{
-        throw new Error(JSON.stringify(schema)+"缺少 type字段");
+        throw new Error(JSON.stringify(schema) + "缺少 type字段");
     }
     //console.log(object_stack);
 
---
+++ src/json-schema-js/lib/getSchemaByPointer.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jjq on 6/23/17.
+ */
+//通过SchemaPointer来获取指定的Schema
+//SchemaPointer的定义仅是与JsonPointer有点相似，但增加了许多特别处理,比如对array、allOf、anyOf的处理等等。
+
+module.exports = function(){
+
+};
